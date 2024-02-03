08ece394998456eb94c9cbda5129278e9397bc7e
evie <e.v.221b@gmail.com>
2024-02-03 21:26:35 +0330

    just testing

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 //////////// SECTION CONSTRUCTOR FUNCTION
 // arrow function dosent work
@@ -9,18 +9,17 @@ const Person = function (firstName, birthYear) {
   //  never creat a method in conctruction function it will be attachted to every obj
   // this.clalcAge = function () {
   //   console.log(2023 - this.birthYear);
-  //
-  //   };
+  // };
 };
 
-const evie = new Person('evie', 2003);
+const evie = new Person("evie", 2003);
 // 1. new {} is created
 // 2.function is clled , this={}
 // 3. {} linked to prototype
 // 4. function automatically return {}
 console.log(evie);
 
-const jonas = new Person('jonas', 1993);
+const jonas = new Person("jonas", 1993);
 console.log(jonas);
 
 console.log(jonas instanceof Person);
@@ -39,7 +38,7 @@ console.log(Person.prototype.isPrototypeOf(evie));
 console.log(Person.prototype.isPrototypeOf(Person));
 //  the prototype is the prototype that is ganna be used by obj not the prototype of person
 
-Person.prototype.species = 'Homo Sapiens';
+Person.prototype.species = "Homo Sapiens";
 console.log(evie.species, jonas.species);
 
-console.log(evie.hasOwnProperty('firstName'));
+console.log(evie.hasOwnProperty("firstName"));
